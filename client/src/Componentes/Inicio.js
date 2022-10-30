754776b3c2339b9b20d27e3cc4d70de636cab9c7
Nicolas Rodriguez <nicorodriguez9900@hotmail.com>
2022-10-30 15:02:41 -0300

    Menu inicial

--- client/src/Componentes/Inicio.js
+++ client/src/Componentes/Inicio.js
@@ -2,37 +2,47 @@ import React from 'react';
 import LogoInicio from '../Images/LogoInicio.jpg';
 import UserImg from '../Images/iconoPerfil.png';
 import Timeline from '../Images/TimelineImg.png';
+import Comidas from '../Images/ComidasImg.png';
+import Informes from '../Images/InformesImg.png';
 import { NavLink } from 'react-router-dom';
 const Inicio = () => {
     return (
         <div className="container">
-            <div class="row">
+            <div class="row " >
                 <div class="col align-self-start">
                     <img src={LogoInicio} className="imgLogo" alt="LogoDL" />
                 </div>   
-                <div class="col align-self-end">
+                <div class="col d-flex align-self-center justify-content-end">
                     <img src={UserImg} className="UserImg" alt="User" />
                 </div>
             </div>
-            <div class="row">
-           
-                <div class="col">
-                    <NavLink className="linkMenu" exact to="/Comidas"  id="dash" >
+            <div class="row padding20">
+                <div class="col d-flex justify-content-center">
+                    <NavLink  exact to="/Comidas"  id="dash" >
+                        <table className="linkContainer" >                    
+                            <img src={Comidas} className="iconosImg" alt="User" />
+                        </table>
+                    </NavLink>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <NavLink exact to="/Timeline"  id="dash" >
                         <table className="linkContainer" >                    
                             <img src={Timeline} className="iconosImg" alt="User" />
                         </table>
                     </NavLink>
                 </div>
-                <div class="col">
-                    <table className="linkContainer" >                    
-                        <NavLink className="linkMenu" exact to="/Timeline"  id="dash" >TimeLine</NavLink>
-                    </table>
+                <div class="col d-flex justify-content-center">
+                    <NavLink  exact to="/Informes"  id="dash" >
+                        <table className="linkContainer" >                    
+                            <img src={Informes} className="iconosImg" alt="User" />
+                        </table>
+                    </NavLink>    
                 </div>
-                <div class="col">
-                    <table className="linkContainer" >                    
-                        <NavLink className="linkMenu" exact to="/Informes"  id="dash" >TimeLine</NavLink>
-                    </table>
-                </div>   
+            </div>
+            <div class="row textosMenuInicial">
+                    <h2 class="col d-flex justify-content-center">Comidas</h2>
+                    <h2 class="col d-flex justify-content-center">Iniciar servicio</h2> 
+                    <h2 class="col d-flex justify-content-center">Informes</h2>
             </div>
         </div>
     )
