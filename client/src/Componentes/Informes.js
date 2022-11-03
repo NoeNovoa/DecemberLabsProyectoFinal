a75f02c8a3120bce893474052315ad6745748565
Maria Noel <maria.noel.novoa.pacilio@gmail.com>
2022-11-02 23:44:56 -0300

    Informes Noe 05

--- client/src/Componentes/Informes.js
+++ client/src/Componentes/Informes.js
@@ -23,7 +23,7 @@ let menuOrdenado = [
 
 let pedidos = [
     { id: 1, nombreMenu: "Bondiola", nombreEmpleado: "Noe", horario: "13:00", diaAlmuerzo: "02/11/2022", esVegetariano: "No" },
-    { id: 2, nombreMenu: "Gramajo", nombreEmpleado: "Nico", horario: "13:30", diaAlmuerzo: "02/11/2022", esVegetariano: "Si" },
+    { id: 2, nombreMenu: "Bondiola", nombreEmpleado: "Nico", horario: "13:30", diaAlmuerzo: "02/11/2022", esVegetariano: "Si" },
     { id: 3, nombreMenu: "Milanesa", nombreEmpleado: "Vice", horario: "14:00", diaAlmuerzo: "02/11/2022", esVegetariano: "No" }
 ]
 
@@ -53,7 +53,7 @@ const Informes = () => {
             <h2 class="col d-flex justify-content-center textosMenuInicial">Informes</h2>
             <div class="row textosMenuInicial">
                 <div class=" card col d-flex justify-content-center">
-                    <h3 class=" justify-content-center tituloInforme">Cantidad de platos elaborados por horario por día</h3>
+                    <h3 class=" justify-content-center tituloInforme">Platos elaborados por horario por día</h3>
                     <label for="fecha" class="divContenido">Fecha Elaboración</label>
                     <input placeholder="Seleccionar fecha" type="date" class="form-control" id="horario"></input><br></br>
                     <label for="horario" class="divContenido">Horario de Almuerzo</label>
@@ -76,6 +76,8 @@ const Informes = () => {
                             {pedidos.map(p => <tr key={p.id}><td key={p.id + "nombre"}>{p.nombreMenu}</td><td key={p.id + "nombreEmpleado"}>{p.nombreEmpleado}</td><td key={p.id + "horario"}>{p.horario}</td><td key={p.id + "dia"}>{p.diaAlmuerzo}</td><td key={p.id + "esVeg"}>{p.esVegetariano}</td></tr>)}
                         </tbody>
                     </table>
+                    <h3 class="divContenido">Cantidad</h3>
+                    <p class="divTexto">La cantidad de platos elaborados en el filtro establecido es de 3.</p>
                 </div>
             </div>
 
