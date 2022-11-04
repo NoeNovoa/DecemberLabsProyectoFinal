860ca0f3ba8d3bbea4e65687c0741f0bab0628b9
Maria Noel <maria.noel.novoa.pacilio@gmail.com>
2022-11-04 00:10:15 -0300

    Graficas 03 Noe

--- client/src/Componentes/Graficas.js
+++ client/src/Componentes/Graficas.js
@@ -20,8 +20,8 @@ ChartJS.register(
     Legend
 );
 
-export const options = {
-    indexAxis: 'y',
+const options = {
+    indexAxis: 'x',
     elements: {
         bar: {
             borderWidth: 2,
@@ -34,37 +34,98 @@ export const options = {
         },
         title: {
             display: true,
-            text: 'Chart.js Horizontal Bar Chart',
+           //text: 'Chart.js Horizontal Bar Chart',
         },
     },
 };
 
-let horarios = [
-    { id: 1, horaDesde: "13:00" },
-    { id: 2, horaDesde: "13:30" },
-    { id: 3, horaDesde: "14:00" }
+let cantidadMenus = [
+    { id: 1, cantidad: "10" },
+    { id: 2, cantidad: "5" },
+    { id: 4, cantidad: "100" },
+    { id: 5, cantidad: "50" },
+    { id: 6, cantidad: "60" },
+    { id: 7, cantidad: "7" },
+    { id: 8, cantidad: "5" },
+    { id: 9, cantidad: "5" },
+    { id: 10, cantidad: "0" },
+    { id: 11, cantidad: "0" },
+    { id: 12, cantidad: "10" }
 ]
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const Graficas = () => {
+    return (
+        <div className="container">
+            <h2 class="col d-flex justify-content-center textosMenuInicial">Graficas</h2>
+            <div class="row textosMenuInicial">
+                <div class="card col d-flex justify-content-center">
+                    <h3 class="col d-flex justify-content-center divContenido">Almuerzos por mes</h3>
+                    <Bar className="grafica" data={{
+                         labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+                        datasets: [
+                            {
+                                label: 'Cant Almuerzos',
+                                data: cantidadMenus.map(p => (p.cantidad)),
+                                backgroundColor: [
+                                    'black',
+                                    'black',
+                                    'black',
+                                    'black',
+                                ],
+                                borderColor: [
+                                    'black',
+                                    'black',
+                                    'black',
+                                    'black',
+                                ],
+                                borderWidth: 1,
+                            },
+                        ],
+                    }} options={options} class="divContenido"/>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <h3 class="col d-flex justify-content-center">Promedio de Almuerzos por mes</h3>
+                </div>
+            </div>
+            <div class="row textosMenuInicial">
+                <div class="col d-flex justify-content-center">
+                    <h3 class="col d-flex justify-content-center">Pedidos por personas</h3>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <h3 class="col d-flex justify-content-center">Picos de hora</h3>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+
+export default Graficas
+
+
+
+
+/*
+OTRO EJEMPLO
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
 export const data = {
-    labels,
+    meses,
     datasets: [
       {
         label: 'Dataset 1',
-        data:  horarios.map(h => <option key={h.id} value={h.id}>{h.horaDesde}</option>),
+        data:  cantidadMenus.map(h => <option key={h.id} value={h.id}>{h.cantidad}</option>),
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
       {
         label: 'Dataset 2',
-        data: horarios.map(h => <option key={h.id} value={h.id}>{h.horaDesde}</option>),
+        data: cantidadMenus.map(h => <option key={h.id} value={h.id}>{h.cantidad}</option>),
         borderColor: 'rgb(53, 162, 235)',
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
     ],
   };
-  
 
 const Graficas = () => {
     return (
@@ -74,9 +135,6 @@ const Graficas = () => {
                 <div class="card col d-flex justify-content-center">
                     <h3 class="col d-flex justify-content-center divContenido">Almuerzos por mes</h3>
                     <Bar options={options} data={data} class="divContenido"/>;
-
-
-
                 </div>
                 <div class="col d-flex justify-content-center">
                     <h3 class="col d-flex justify-content-center">Cantidad de Almuerzos por mes</h3>
@@ -100,53 +158,8 @@ export default Graficas
 
 
 
-/*
 
 
 
-
-
-
-
-const Graficas = () => {
-    return (
-        <div className="container">
-            <h2 class="col d-flex justify-content-center textosMenuInicial">Graficas</h2>
-            <div class="row textosMenuInicial">
-                <div class="col d-flex justify-content-center">
-                    <h3 class="col d-flex justify-content-center">Almuerzos por mes</h3>
-
-            <Bar className="grafica" data={{
-                labels: ['1','2','2','4','5','6','7','8','9','10','11','12'],
-                datasets: [
-                    {
-                        label: 'Mes',
-                        data: ['1','2','2','4','5','6','7','8','9','10','11','12'],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                        ],
-                        borderColor: [
-                            'black',
-                            'black',
-                            'black',
-                            'black',
-                        ],
-                        borderWidth: 1,
-                    },
-                ],
-            }} options={options} />
-            <h3 class="col d-flex justify-content-center">Cantidad de Almuerzos por mes</h3>
-            </div>       
-        </div>
-        <div class="row textosMenuInicial">   
-            <div class="col d-flex justify-content-center">
-                <h3 class="col d-flex justify-content-center">Pedidos por personas</h3>
-                <h3 class="col d-flex justify-content-center">Picos de hora</h3>
-            </div>   
-        </div>
-    </div>
 
 */
