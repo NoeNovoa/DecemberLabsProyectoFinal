94e8f5d74a51b1d1b593350a59c5efdf8ac83839
Maria Noel <maria.noel.novoa.pacilio@gmail.com>
2022-11-12 01:16:11 -0300

    front login  + mvc menu + routes

--- server/server.js
+++ server/server.js
@@ -1,23 +1,9 @@
 const express = require('express');
-const cors = require('cors');
-const db = require('../database/db');
-const routes = require('../routes/routes.js');
+//const cors = require('cors');
+//const db = require('../database/db');
+//const routes = require('../routes/routes.js');
 
 const app = express();
-
-//const {conectar} = require('./Utilidades/config.js');
-app.use(cors())
-app.use(express.json())
-app.use('/menus',routes)
-
-try{
-    await db.authenticate()
-    console.log('Conexion exitosa a la DB')
-}
-catch (error){
-    console.log(`El error de conexion es: ${error}`)
-}
-
 
 // Comment Nico
 app.get("/api", (req, res) =>{
